0b49420bb48562ab4a26e1484db039e9c2f5b89d
venkat020401 <venkat020401@gmail.com>
2022-11-29 15:42:41 +0530

    reactday1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./card";
 
 function App() {
+  var pricecard = [
+    {
+      price: 0,
+      plan: "free",
+      features: [
+        {
+          highlight: false,
+          plan: "Single User"
+        },
+        {
+          plan: "5GB Storage"
+        },
+        {
+          plan: "Unlimited Public Projects"
+        },
+        {
+          plan: "Community Access"
+        },
+        {
+          textMute : true,
+          plan: "Unlimited Private Projects"
+        },
+        {
+          textMute : true,
+          plan: "Dedicated Phone Support"
+        },
+        {
+          textMute : true,
+          plan: "Free Subdomain"
+        },
+        {
+          textMute : true,
+          plan: "Monthly Status Reports"
+        },
+      ],
+    },
+    {
+      price: 9,
+      plan: "plus",
+      features: [
+        {
+          highlight: true,
+          plan: "5 Users"
+        },
+        {
+          plan: "50GB Storage"
+        },
+        {
+          plan: "Unlimited Public Projects"
+        },
+        {
+          plan: "Community Access"
+        },
+        {
+          plan: "Unlimited Private Projects"
+        },
+        {
+          plan: "Dedicated Phone Support"
+        },
+        {
+          plan: "Free Subdomain"
+        },
+        {
+          textMute : true,
+          plan: "Monthly Status Reports"
+        },
+      ],
+    },
+    {
+      price: 49,
+      plan: "pro",
+      features: [
+        {
+          highlight: true,
+          plan: "Unlimited Users"
+        },
+        {
+          plan: "150GB Storage"
+        },
+        {
+          plan: "Unlimited Public Projects"
+        },
+        {
+          plan: "Community Access"
+        },
+        {
+          plan: "Unlimited Private Projects"
+        },
+        {
+          plan: "Dedicated Phone Support"
+        },
+        {
+          plan: "Unlimited Free Subdomains"
+        },
+        {
+          plan: "Monthly Status Reports"
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {pricecard.map((card) => {
+            return <Card card={card}></Card>;
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
---
+++ src/card.js
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+function Card({ card }) {
+  return (
+    <div class="col-lg-4">
+      <div class="card mb-5 mb-lg-0">
+        <div class="card-body">
+          <h5 class="card-title text-muted text-uppercase text-center">
+            {card.plan}
+          </h5>
+          <h6 class="card-price text-center">
+            ${card.price}
+            <span class="period">/month</span>
+          </h6>
+          <hr />
+          <ul className="fa-ul">
+            {card.features.map((item) => {
+              return <li className={item.textMute ? "text-muted" : ""}>
+                <span class="fa-li">
+                {item.textMute ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faCheck} />}</span>
+                {item.highlight ? <strong>{item.plan}</strong> : item.plan}</li>
+            })}
+          </ul>
+          <div class="d-grid">
+            <a href="#" class="btn btn-primary text-uppercase">
+              Button
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
